41cea7c25726aa4968cbc8c47c76b9b4cb1bcf1d
Oliver Scotten <revi99@me.com>
2023-03-03 07:18:21 -0600

    Update Graduation Date

--- docs/assets/js/graduated.js
+++ docs/assets/js/graduated.js
@@ -4,7 +4,7 @@ function isInThePast(date) {
     return date < today;
   }
   
-  if (isInThePast(new Date('2023-06-09'))) {
+  if (isInThePast(new Date('2023-03-17'))) {
       document.write("He graduated from DePaul University in 2023 with a major in Sound Design from The Theatre School and a minor in Network Technology from the college of Computing and Digital Media.")
   } 
   else {
